e7d65ed72c0c694e9c34454546c768ec1cfceaa7
JasonFaas <jason.a.faas@gmail.com>
2016-08-10 19:48:43 -0700

    Chapter 1 files added

---
+++ src/main/scala/ChapterOne.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by jasonfaas on 8/10/16.
+  */
+object ChapterOne {
+  def getHelloWorld(): Unit = {
+    return "Hello Chapter 1"
+  }
+
+}
---
+++ src/test/scala/ChapterOneSpec.scala
@@ -0,0 +1,16 @@
+import org.scalatest.FunSpec
+
+/**
+  * Created by jasonfaas on 8/10/16.
+  */
+class ChapterOneSpec extends FunSpec{
+  describe("Is Chapter 1 Real?") {
+    it("should equals 2"){
+      assert(1+1 == 2)
+    }
+
+    it("Should call another method") {
+      assert(ChapterOne.getHelloWorld() == "Hello Chapter 1");
+    }
+  }
+}
